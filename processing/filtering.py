4726e550768f8d51e1fe38e5b5ef984b20064be0
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-06-27 00:58:18 +0300

    Using initiator instead of maintainer for filtering
    + small refactorings and bugfixes

--- processing/filtering.py
+++ processing/filtering.py
@@ -8,20 +8,20 @@ logger = logging.getLogger(__name__)
 USER_SET_KEY = 'filtered-users-set'
 
 def filter_out(context: Context, event: Dict) -> bool:
-    maintainer_id = event.get('dataset_obj', {}).get('maintainer')
-    filtered_out = _filter_out_users(context, maintainer_id)
+    username = event.get('initiator_user_name')
+    filtered_out = _filter_out_users(context, username)
     return filtered_out
 
 
-def _filter_out_users(context: Context, user_id: str):
-    if user_id:
+def _filter_out_users(context: Context, username: str):
+    if username:
         if not context.store.exists(USER_SET_KEY):
-            user_ids = fetch_users(context.gsheets, context.config)
-            if user_ids:
-                context.store.set_set(USER_SET_KEY, user_ids)
+            usernames = fetch_users(context.gsheets, context.config)
+            if usernames:
+                context.store.set_set(USER_SET_KEY, usernames)
             else:
                 logger.error('User list from google spreadsheet is empty')
-        if context.store.is_in_set(USER_SET_KEY, user_id):
+        if context.store.is_in_set(USER_SET_KEY, username):
             return True
     return False
 
