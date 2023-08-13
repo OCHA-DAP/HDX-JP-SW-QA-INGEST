330c84f9ba41fe352e36fe4e19400f7f2231277f
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-08-14 00:27:31 +0300

    HDX-9089 initial pytest config and tests
    tests for safelists and caching

---
+++ tests/util.py
@@ -0,0 +1,16 @@
+from gspread.client import Client
+
+
+def fetch_values_wrapper(gsheets: Client, spreadsheet_name: str, worksheet_name: str, worksheet_col: int):
+    if worksheet_name == 'User Safelist':
+        return [
+            'blocked-test-user'
+        ]
+    elif worksheet_name == 'Dataset Safelist':
+        return [
+            'blocked-test-dataset-id'
+        ]
+    elif worksheet_name == 'Organization Safelist':
+        return [
+            'blocked-test-org-id'
+        ]
