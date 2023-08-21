f2739e4d494abea77170cd41be7553b78f9fd11e
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-08-21 22:09:06 +0000

    fix for setting timestamp in redis when it does not exist

--- processing/limit_batches.py
+++ processing/limit_batches.py
@@ -48,7 +48,7 @@ def limit(context: Context, event: Dict) -> bool:
         context.slack_client.post_to_slack_channel(limiting_message)
 
         # if there's no batch of datasets in redis then we need to update the timestamp to say that we're starting now
-        if not context.store.exists(batches_datasets_redis_key):
+        if not context.store.exists(batches_datasets_redis_key) or not context.store.exists(batches_timestamp_redis_key):
             context.store.set_object(batches_timestamp_redis_key, timestamp)
         # push the new dataset dict
         context.store.set_map(batches_datasets_redis_key, {event.get('dataset_id'): json.dumps(cached_data)})
